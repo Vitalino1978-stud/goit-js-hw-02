fa2f9f23d4cbf709bc4489c1bdf92e632766c4b3
Vitalino78 <vitalinos1978@gmail.com>
2020-12-07 23:31:56 +0200

    3. adds task-7.js

--- js/task-7.js
+++ js/task-7.js
@@ -5,25 +5,29 @@ const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
 const isLoginValid = function (login) {
   
   if (login.length >= 4 && login.length <= 16) {
-    return true;
+    return false;
   }
-  return false;
-  
+  return true;
 };
 
 const isLoginUnique = function (allLogins, login) {
-  for (let i = 0; i < logins.length; i++) {
-    if (allLogins.includes(logins[i]))
-      return false;
-  }
-  return true;
-  
+  // for (let i = 0; i < logins.length; i++) {
+  //   if (allLogins.includes(logins[i]))
+  //     return false;
+  // }
+  // return true;
+  return allLogins.includes(login);
 };
-
 const addLogin = function(allLogins, login) {
-  if (isLoginValid = false) {
-    console.log('Ошибка! Логин должен быть от 4 до 16 символов');
- }
+  if (isLoginValid (login) === true) {
+    return ('Ошибка! Логин должен быть от 4 до 16 символов');
+  }
+  if (isLoginUnique(allLogins, login) === true) {
+    return('Такой логин уже используется!');
+  } else {
+  allLogins.push(login);
+ return('Логин успешно добавлен!');
+  }
 };
 
 /*
